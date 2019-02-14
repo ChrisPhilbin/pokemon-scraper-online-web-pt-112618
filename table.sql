34e87beb6db112014d22f75971ed27261352581a
Chris Philbin <chris@chrisphilbin.net>
2019-02-14 01:00:10 +0000

    Automatically backed up by Learn

---
+++ table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE juice (
+  id INTEGER PRIMARY KEY,
+  brand TEXT,
+  price INTEGER,
+  flavor TEXT
+);
+
+INSERT INTO juice (brand, price, flavor) VALUES ("Motts", 4, "Apple");
+INSERT INTO juice (brand, price, flavor) VALUES ("Sunny-D", 2, "Orange");
+INSERT INTO juice (brand, price, flavor) VALUES ("Tropicanna", 5, "Orange");
